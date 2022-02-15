88f21f269cd0d3071540303b71566224c31bf776
jun <884720982@qq.com>
2022-02-15 16:10:30 +0800

    修改人：Jun 修改内容： 1、提交项目源码

---
+++ src/main/java/com/bbs/utils/Md5Util.java
@@ -0,0 +1,22 @@
+package com.bbs.utils;
+
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.springframework.util.DigestUtils;
+
+/**
+ * md5加密工具类
+ */
+public class Md5Util {
+    //盐，用于混交md5
+    private static final String slat = "&%5123***&&%%$$#@";
+
+    /**
+     * 生成md5
+     * @param str
+     * @return
+     */
+    public static String getMD5(String str) {
+
+        return new Md5Hash(str, slat, 1024).toString();
+    }
+}
